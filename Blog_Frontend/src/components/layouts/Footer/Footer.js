c4645cee9a8c14450556847656f488e12ea3fa01
Aabid <zainulaabid0007@gmail.com>
2024-11-25 21:05:36 +0530

    final

--- Blog_Frontend/src/components/layouts/Footer/Footer.js
+++ Blog_Frontend/src/components/layouts/Footer/Footer.js
@@ -6,14 +6,14 @@ const Footer = ({ currentPage, totalPages, onPageChange }) => {
   const navigate = useNavigate();
 const AdminPanelClick=() =>(
   navigate('/admin')
-)
+);
   return (
     <div className='footerSection'>
       <div className='container'>
         <footer>
           <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={onPageChange} />
           <p>Powered by Stackup </p>
-          <button onClick={AdminPanelClick()}>Admin Panel</button>
+          <button onClick={ () => AdminPanelClick()}>Admin Panel</button>
         </footer>
       </div>
     </div>
