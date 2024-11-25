59304738ae2104792f94f67bc935ec47502885d2
Aabid <zainulaabid0007@gmail.com>
2024-11-25 21:42:40 +0530

    final

--- Blogs-backend/server.js
+++ Blogs-backend/server.js
@@ -3,16 +3,17 @@ const errorHandler = require("./middleware/errorHandler");
 const connectDb = require("./config/dbConnection");
 const dotenv = require("dotenv").config();
 const cors = require("cors");
+const path = require('path');
 
 connectDb();
 const app = express();
 const port = process.env.PORT || 3000;
-const path = require('path');
+
 
 app.use(cors({ origin: "https://mini-blog-b7ik.onrender.com" }));
 app.use(express.json());
 app.use("/api/blogs", require("./routes/blogsRoutes"));
-app.use(errorHandler);
+
 
 
 app.use(express.static(path.join(__dirname, 'build')));
@@ -20,6 +21,7 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+app.use(errorHandler);
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
