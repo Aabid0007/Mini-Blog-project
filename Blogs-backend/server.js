6ca8b0d6df3e76e8f3b48269beef0c3aa1b55aee
Aabid <zainulaabid0007@gmail.com>
2024-11-25 20:35:56 +0530

    final

--- Blogs-backend/server.js
+++ Blogs-backend/server.js
@@ -9,7 +9,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 
-app.use(cors());
+app.use(cors({ origin: "https://blog-z3i4.onrender.com" }));
 app.use(express.json());
 app.use("/api/blogs", require("./routes/blogsRoutes"));
 app.use(errorHandler);
