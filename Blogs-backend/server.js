f6920c90d8475b5119a54344575ab8590e29ff91
Aabid <zainulaabid0007@gmail.com>
2024-11-25 21:07:04 +0530

    final

--- Blogs-backend/server.js
+++ Blogs-backend/server.js
@@ -7,7 +7,7 @@ const cors = require("cors");
 connectDb();
 const app = express();
 const port = process.env.PORT || 3000;
-
+const path = require('path');
 
 app.use(cors({ origin: "https://mini-blog-b7ik.onrender.com" }));
 app.use(express.json());
@@ -15,6 +15,11 @@ app.use("/api/blogs", require("./routes/blogsRoutes"));
 app.use(errorHandler);
 
 
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
