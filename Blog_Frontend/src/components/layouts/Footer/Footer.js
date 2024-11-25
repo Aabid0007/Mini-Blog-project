b0d1e501df26ed26924109d3c34a2bda467a83ac
Aabid <zainulaabid0007@gmail.com>
2024-11-26 00:03:20 +0530

    final

--- Blog_Frontend/src/components/layouts/Footer/Footer.js
+++ Blog_Frontend/src/components/layouts/Footer/Footer.js
@@ -4,16 +4,17 @@ import Pagination from '../../pagination/Pagination'
 import { useNavigate } from 'react-router-dom';
 const Footer = ({ currentPage, totalPages, onPageChange }) => {
   const navigate = useNavigate();
-const AdminPanelClick=() =>(
-  navigate('/admin')
-);
+  const AdminPanelClick = () => (
+    navigate('/admin')
+  );
   return (
     <div className='footerSection'>
       <div className='container'>
         <footer>
           <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={onPageChange} />
-          <p>Powered by Stackup </p>
-          <button className='AdminBtn' onClick={AdminPanelClick}>Admin Panel</button>
+          <p>Powered by Stackup
+            <button className='AdminBtn' onClick={AdminPanelClick}>Admin Panel</button>
+          </p>
         </footer>
       </div>
     </div>
